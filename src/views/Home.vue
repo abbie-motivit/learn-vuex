3378ad4f3e4e65955fb69a3dbc78721c35d3d2a8
abbie <abbie.mercado@motivit.com>
2021-11-18 15:17:36 +0800

    -- create counter app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <counter/>
+    <counterSquared/>
+    <buttons/>
+    <colorCode/>
   </div>
 </template>
 
 <script>
+import Counter from '../components/Counter.vue'
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    'counter': require('@/components/Counter.vue').default,
+    'counterSquared': require('@/components/CounterSquared.vue').default,
+    'buttons': require('@/components/Buttons.vue').default,
+    'colorCode': require('@/components/ColorCode.vue').default
   }
 }
 </script>
+
+<style >
+  div{
+    margin-bottom: 10px;
+  }
+  .counter {
+    font-size: 80px;
+  }
+  .buttons button {
+    font-size: 40px;
+    width: 100px;
+  }
+</style>
